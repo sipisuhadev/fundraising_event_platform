a2f38f8224f7355ec4b08a0c81aa03687e99a086
sipisuhadev <131577250+sipisuhadev@users.noreply.github.com>
2023-10-25 14:03:57 +0530

    Update settings.py

--- fundraiser/settings.py
+++ fundraiser/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*!9vh)$rril@4m9=$ypr^oo*h)al5pr^6fb3jbb1#el9oj#us5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
